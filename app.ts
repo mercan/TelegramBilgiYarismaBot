02a218cda72a217cc4f7c51d25f191032f506c76
mercan <imrcn77@gmail.com>
2023-03-17 11:12:07 +0300

    add: express framework

--- app.ts
+++ app.ts
@@ -73,3 +73,16 @@ const onExpired = async (channel: string, message: string) => {
 
 // Redis expired key event
 RedisService.onQuizExpired(onExpired);
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
