ef8d4ea9dd9f6a1db03b13449030af2a42d26350
mercan <imrcn77@gmail.com>
2024-04-22 20:21:48 +0300

    .

--- app.ts
+++ app.ts
@@ -91,5 +91,5 @@ app.get("/", (req: express.Request, res: express.Response) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port ", port);
 });
